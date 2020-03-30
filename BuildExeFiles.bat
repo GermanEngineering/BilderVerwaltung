e356697485847705712c1d38b2e620508a2174ea
peter <pkrueckel@gmail.com>
2020-03-30 14:05:58 +0200

    Update Build .bat

--- BuildExeFiles.bat
+++ BuildExeFiles.bat
@@ -3,8 +3,8 @@ cd /d D:\tmp\xxx\Projects\BilderVerwaltung\VENV_BilderVerwaltung\Scripts
 REM Activate Environment and run pyinstaller command
 activate.bat^
  & cd /d D:\tmp\xxx\Projects\BilderVerwaltung^
- & pyinstaller -F -i "Icons\UnifyPictureNames.ico" "UnifyPictureNames.py" --version-file=Version.py^
- & pyinstaller -F -i "Icons\SetNameAsCaptureDate.ico" "SetNameAsCaptureDate.py" --version-file=Version.py^
- & pyinstaller -F -i "Icons\ReduceImageFileSize.ico" "ReduceImageFileSize.py" --version-file=Version.py^
- & pyinstaller -F -i "Icons\ChangeCaptureDate.ico" "ChangeCaptureDate.py" --version-file=Version.py^
+ & pyinstaller --onefile --icon "Icons\UnifyPictureNames.ico" "UnifyPictureNames.py" --version-file=Version.py --name "Bilder_Namen_Vereineitlichen.py"^
+ & pyinstaller --onefile --icon "Icons\SetNameAsCaptureDate.ico" "SetNameAsCaptureDate.py" --version-file=Version.py --name "Name_Als_Aufnahmedatum.py"^
+ & pyinstaller --onefile --icon "Icons\ReduceImageFileSize.ico" "ReduceImageFileSize.py" --version-file=Version.py --name "Dateigröße_Reduzieren.py"^
+ & pyinstaller --onefile --icon "Icons\ChangeCaptureDate.ico" "ChangeCaptureDate.py" --version-file=Version.py --name "Aufnahmedatum_Anpassen.py"^
  & PAUSE
