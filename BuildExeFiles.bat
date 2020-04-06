58624639fe0854db5ccae0ea7006b5ab8d9522c2
GermanEngineering <peter@trustmeimanengineer.de>
2020-04-06 19:54:03 +0200

    German executable names.

--- BuildExeFiles.bat
+++ BuildExeFiles.bat
@@ -1,10 +1,12 @@
-REM Change to Environment directory.
-cd /d D:\tmp\xxx\Projects\BilderVerwaltung\VENV_BilderVerwaltung\Scripts
+REM Change to Environment directory (location of .bat file).
+cd /D "%~dp0"
+REM cd VENV_BilderVerwaltung\Scripts
+cd VENV_BilderVerwaltung_T440s\Scripts
 REM Activate Environment and run pyinstaller command
 activate.bat^
- & cd /d D:\tmp\xxx\Projects\BilderVerwaltung^
+ & cd /D "%~dp0"^
  & pyinstaller --onefile --icon "Icons\UnifyPictureNames.ico" "UnifyPictureNames.py" --version-file=Version.py --name "Bilder_Namen_Vereineitlichen.py"^
  & pyinstaller --onefile --icon "Icons\SetNameAsCaptureDate.ico" "SetNameAsCaptureDate.py" --version-file=Version.py --name "Name_Als_Aufnahmedatum.py"^
- & pyinstaller --onefile --icon "Icons\ReduceImageFileSize.ico" "ReduceImageFileSize.py" --version-file=Version.py --name "Dateigröße_Reduzieren.py"^
+ & pyinstaller --onefile --icon "Icons\ReduceImageFileSize.ico" "ReduceImageFileSize.py" --version-file=Version.py --name "Dateigroesse_Reduzieren.py"^
  & pyinstaller --onefile --icon "Icons\ChangeCaptureDate.ico" "ChangeCaptureDate.py" --version-file=Version.py --name "Aufnahmedatum_Anpassen.py"^
  & PAUSE
